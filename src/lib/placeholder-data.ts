4f3bf8c8fbb3a36364e013c76072fdb126bdc61e
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2024-08-04 20:00:13 +0100

    Seed file and placeholder

--- src/lib/placeholder-data.ts
+++ src/lib/placeholder-data.ts
@@ -1,147 +1,123 @@
-// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
-// https://nextjs.org/learn/dashboard-app/fetching-data
+// This file contains placeholder data
 const users = [
   {
     id: "410544b2-4001-4271-9855-fec4b6a6442a",
-    name: "User",
+    first_name: "John",
+    last_name: "Doe",
+    profile_image:
+      "https://www.pexels.com/photo/portrait-photo-of-man-1722198/",
     email: "user@nextmail.com",
     password: "123456",
   },
 ];
 
-const customers = [
+const communities = [
   {
     id: "d6e15727-9fe1-4961-8c5b-ea44a9bd81aa",
-    name: "Evil Rabbit",
-    email: "evil@rabbit.com",
-    image_url: "/customers/evil-rabbit.png",
+    name: "Fellow Humans",
+    slug: "fellow-humans",
+    theme: {
+      font: "placeholder",
+      primary_colour: "#1501f5",
+      secondary_colour: "#FFFFFF",
+    },
+    description: "Curated Events Bringing Humans Together",
+    logo: "https://assets.bigcartel.com/account_images/6694384/Fellow+Humans+LogoNEWNEWq.png?auto=format&fit=max&h=1200&w=1200",
+    cover_image:
+      "https://images.pexels.com/photos/2788792/pexels-photo-2788792.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   },
   {
     id: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
-    name: "Delba de Oliveira",
-    email: "delba@oliveira.com",
-    image_url: "/customers/delba-de-oliveira.png",
+    name: "People Planet Pint",
+    slug: "people-planet-pint",
+    theme: {
+      font: "placeholder",
+      primary_colour: "#2351d5",
+      secondary_colour: "#FFFFFF",
+    },
+    description:
+      "No agenda, workshops or webinars. Just chat over some drinks. Pop down to your local People Planet Pint to find more about what’s going on locally with sustainability and how you can get involved.",
+    logo: "https://media.licdn.com/dms/image/C4D0BAQFxKDzjDWpI0g/company-logo_200_200/0/1630537682326/small99_logo?e=2147483647&v=beta&t=simed9nZldId-2e2c8NLVw-TH-PeuJThE4FAfk7uhDI",
+    cover_image:
+      "https://images.pexels.com/photos/1267697/pexels-photo-1267697.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   },
   {
     id: "3958dc9e-742f-4377-85e9-fec4b6a6442a",
-    name: "Lee Robinson",
-    email: "lee@robinson.com",
-    image_url: "/customers/lee-robinson.png",
-  },
-  {
-    id: "76d65c26-f784-44a2-ac19-586678f7c2f2",
-    name: "Michael Novotny",
-    email: "michael@novotny.com",
-    image_url: "/customers/michael-novotny.png",
-  },
-  {
-    id: "CC27C14A-0ACF-4F4A-A6C9-D45682C144B9",
-    name: "Amy Burns",
-    email: "amy@burns.com",
-    image_url: "/customers/amy-burns.png",
-  },
-  {
-    id: "13D07535-C59E-4157-A011-F8D2EF4E0CBB",
-    name: "Balazs Orban",
-    email: "balazs@orban.com",
-    image_url: "/customers/balazs-orban.png",
+    name: "Clyde Mountaineering Club",
+    slug: "clydemc",
+    theme: {
+      font: "placeholder",
+      primary_colour: "#060d31",
+      secondary_colour: "#FFFFFF",
+    },
+    description:
+      "A group of friendly, kind and like-minded people who like to get out and have challenging days in the mountains, safely.",
+    logo: "https://clydemc.org/media/uploads/photos/60d4c191-5308-474d-a10d-5465b3570e91.jpeg",
+    cover_image:
+      "https://images.pexels.com/photos/618833/pexels-photo-618833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   },
 ];
 
-const invoices = [
+const events = [
   {
-    customer_id: customers[0].id,
-    amount: 15795,
-    status: "pending",
-    date: "2022-12-06",
-  },
-  {
-    customer_id: customers[1].id,
-    amount: 20348,
-    status: "pending",
-    date: "2022-11-14",
-  },
-  {
-    customer_id: customers[4].id,
-    amount: 3040,
-    status: "paid",
-    date: "2022-10-29",
-  },
-  {
-    customer_id: customers[3].id,
-    amount: 44800,
-    status: "paid",
-    date: "2023-09-10",
-  },
-  {
-    customer_id: customers[5].id,
-    amount: 34577,
-    status: "pending",
-    date: "2023-08-05",
-  },
-  {
-    customer_id: customers[2].id,
-    amount: 54246,
-    status: "pending",
-    date: "2023-07-16",
-  },
-  {
-    customer_id: customers[0].id,
-    amount: 666,
-    status: "pending",
-    date: "2023-06-27",
-  },
-  {
-    customer_id: customers[3].id,
-    amount: 32545,
-    status: "paid",
-    date: "2023-06-09",
-  },
-  {
-    customer_id: customers[4].id,
-    amount: 1250,
-    status: "paid",
-    date: "2023-06-17",
-  },
-  {
-    customer_id: customers[5].id,
-    amount: 8546,
-    status: "paid",
-    date: "2023-06-07",
-  },
-  {
-    customer_id: customers[1].id,
-    amount: 500,
-    status: "paid",
-    date: "2023-08-19",
-  },
-  {
-    customer_id: customers[5].id,
-    amount: 8945,
-    status: "paid",
-    date: "2023-06-03",
-  },
-  {
-    customer_id: customers[2].id,
-    amount: 1000,
-    status: "paid",
-    date: "2022-06-05",
+    id: "3958dc9e-712f-4377-85e9-fec4b6b6442a",
+    community_id: communities[0].id,
+    title: "Board Games Night",
+    link: "www.google.com",
+    description:
+      "Want to make friends and paint Tote Bags?! Join us while we have a wholesome evening of tote bag painting + connection! They'll be good music, refreshing soft drinks and very cool people.",
+    location: {
+      online_link: "www.google.com",
+      address: "123 Moors Way",
+    },
+    start_timestamp: "2024-07-21T17:30:00Z",
+    end_timestamp: "2024-07-21T19:30:00Z",
   },
 ];
 
-const revenue = [
-  { month: "Jan", revenue: 2000 },
-  { month: "Feb", revenue: 1800 },
-  { month: "Mar", revenue: 2200 },
-  { month: "Apr", revenue: 2500 },
-  { month: "May", revenue: 2300 },
-  { month: "Jun", revenue: 3200 },
-  { month: "Jul", revenue: 3500 },
-  { month: "Aug", revenue: 3700 },
-  { month: "Sep", revenue: 2500 },
-  { month: "Oct", revenue: 2800 },
-  { month: "Nov", revenue: 3000 },
-  { month: "Dec", revenue: 4800 },
+const photos = [
+  {
+    id: "3e5d1e1b-b2d5-4f5b-91c8-1d234c1e94b1",
+    community_id: communities[0].id,
+    event_id: events[0].id,
+    url: "https://images.pexels.com/photos/20827831/pexels-photo-20827831/free-photo-of-person-fingers-holding-dandelion-on-meadow.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    alt_text: "Person Fingers Holding Dandelion on Meadow",
+  },
 ];
 
-export { users, customers, invoices, revenue };
+const community_members = [
+  {
+    id: "d6e15727-9fe1-4961-8c5b-ea44a2bd81aa",
+    community_id: communities[0].id,
+    user_id: users[0].id,
+  },
+];
+
+const community_committee_members = [
+  {
+    id: "7f9e0b75-3d5d-456f-a5c3-ffed4ef6c2b1",
+    community_id: communities[0].id,
+    community_member_id: community_members[0].id,
+    role: "Admin",
+  },
+];
+
+const community_documents = [
+  {
+    id: "9d1e1e1b-7d5d-4f5b-a1c8-2d234d1e94b2",
+    community_id: communities[0].id,
+    doc_type: "link",
+    markdown_text: "",
+    link_url: "www.google.com",
+  },
+];
+
+export {
+  users,
+  communities,
+  events,
+  photos,
+  community_members,
+  community_committee_members,
+  community_documents,
+};
