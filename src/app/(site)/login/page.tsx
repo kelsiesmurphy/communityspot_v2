2e7a609a1f81aa0c708a7c17cd9e5fb9f4b0f5f4
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2024-08-06 22:51:59 +0100

    Adding basic supabase auth

--- src/app/(site)/login/page.tsx
+++ src/app/(site)/login/page.tsx
@@ -1,11 +1,14 @@
-import LoginForm from "@/components/site/login-form";
+import { login, signup } from "./actions";
 
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <LoginForm />
-      </div>
-    </main>
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input id="email" name="email" type="email" required />
+      <label htmlFor="password">Password:</label>
+      <input id="password" name="password" type="password" required />
+      <button formAction={login}>Log in</button>
+      <button formAction={signup}>Sign up</button>
+    </form>
   );
 }
